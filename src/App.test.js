9db26485a227a61f702b4f39211f69c6712ac7f6
steady <inchang0326@naver.com>
2022-12-11 21:41:23 +0900

    ESLint Setting

--- src/App.test.js
+++ src/App.test.js
@@ -13,6 +13,13 @@ test("renders learn react link", () => {
     </BrowserRouter>
   );
   const linkElement = screen.getByText(/상품 업로드/i);
+  /*
+    getBy..()   : 찾는 요소가 없으면 오류 발생, 둘 이상의 요소가 발견되면 오류 발생(=> getAllBy..())
+    queryBy..() : 찾는 요소가 없으면 null 반환, 둘 이상의 요소가 발견되면 오류 발생(=> queryAllBy..())
+    findBy..()  : 찾는 요소가 없으면 오류 발생, 둘 이상의 요소가 발견되면 오류 발생(=> findAllBy..())
+                  요소가 발견되면 다른 것과 달리, Promise 반환. 기본 제한시간인 1000ms.
+                  findBy..() = getBy..() + waitFor(일정 시간 동안 기다림)
+  */
   expect(linkElement).toBeInTheDocument();
   console.log(`=> ${linkElement}`);
 });
